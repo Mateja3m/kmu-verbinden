288134b8bbb28b74b2c4581b37784091db37e360
Mateja3m <milanmatejicdev@gmail.com>
2025-07-01 11:52:33 +0200

    Add Schweizer Digitalrat  to partners

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -1,218 +1,224 @@
-
-import { FrontendPartner } from '@/types/database/partners';
+import { FrontendPartner } from "@/types/database/partners";
 
 export const nationalPartners: FrontendPartner[] = [
   {
-    id: 'fasoon',
-    name: 'Fasoon',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1742885445/Logo_Fasoon_rgb-transparent_de_tzaymm.png',
-    website: 'https://fasoon.ch',
-    description: 'Gründungspartner für KMU'
+    id: "fasoon",
+    name: "Fasoon",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1742885445/Logo_Fasoon_rgb-transparent_de_tzaymm.png",
+    website: "https://fasoon.ch",
+    description: "Gründungspartner für KMU",
   },
   {
-    id: 'wirtschaftszeit',
-    name: 'Wirtschaftszeit',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1742885483/WhatsApp_Image_2025-03-25_at_14.02.45_b9kiqi.jpg',
-    website: 'https://www.wirtschaftszeit.ch/',
-    description: 'Publikationsorgan des SKV'
+    id: "wirtschaftszeit",
+    name: "Wirtschaftszeit",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1742885483/WhatsApp_Image_2025-03-25_at_14.02.45_b9kiqi.jpg",
+    website: "https://www.wirtschaftszeit.ch/",
+    description: "Publikationsorgan des SKV",
   },
   {
-    id: 'hhomepage',
-    name: 'hhomepage.ch',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1740493274/hhomepage_Logo-FullColor_bbjtv2.png',
-    website: 'https://hhomepage.ch',
-    description: 'Webdesign und professionelle Online-Auftritte für KMU'
+    id: "hhomepage",
+    name: "hhomepage.ch",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1740493274/hhomepage_Logo-FullColor_bbjtv2.png",
+    website: "https://hhomepage.ch",
+    description: "Webdesign und professionelle Online-Auftritte für KMU",
   },
   {
-    id: 'sd-treuhand',
-    name: 'SD Treuhand & Dienste',
-    logo: 'https://www.sdtreuhandunddienste.ch/wp-content/themes/sdtreuhandf2017/files/images/logo.png',
-    website: 'https://www.sdtreuhandunddienste.ch',
-    description: 'Treuhand, Buchhaltung und Steuerberatung'
+    id: "sd-treuhand",
+    name: "SD Treuhand & Dienste",
+    logo: "https://www.sdtreuhandunddienste.ch/wp-content/themes/sdtreuhandf2017/files/images/logo.png",
+    website: "https://www.sdtreuhandunddienste.ch",
+    description: "Treuhand, Buchhaltung und Steuerberatung",
   },
   {
-    id: 'kmu-digitalisierung',
-    name: 'KMU-Digitalisierung.agency',
-    logo: 'https://cdn-bafdd.nitrocdn.com/LvDfdTjCuEDwNloTwsHTnhZhhGxGiZre/assets/images/optimized/rev-ecf42e5/kmu-digitalisierung.agency/wp-content/uploads/2021/01/KMU-Digitalisierung-Logo-Transparent-300x105.png',
-    website: 'https://kmu-digitalisierung.agency',
-    description: 'Digitalisierungslösungen und Prozessautomatisierung'
+    id: "kmu-digitalisierung",
+    name: "KMU-Digitalisierung.agency",
+    logo: "https://cdn-bafdd.nitrocdn.com/LvDfdTjCuEDwNloTwsHTnhZhhGxGiZre/assets/images/optimized/rev-ecf42e5/kmu-digitalisierung.agency/wp-content/uploads/2021/01/KMU-Digitalisierung-Logo-Transparent-300x105.png",
+    website: "https://kmu-digitalisierung.agency",
+    description: "Digitalisierungslösungen und Prozessautomatisierung",
   },
   {
-    id: 'netcare',
-    name: 'netcare.ch',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBKepsWYcvYb80UVhU80swMiDUvQWMjPcZTw&s',
-    website: 'https://netcare.ch',
-    description: 'IT-Support und Netzwerksicherheit'
+    id: "netcare",
+    name: "netcare.ch",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBKepsWYcvYb80UVhU80swMiDUvQWMjPcZTw&s",
+    website: "https://netcare.ch",
+    description: "IT-Support und Netzwerksicherheit",
   },
   {
-    id: 'kensignton',
-    name: 'Kontour Design Studio',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1739987898/klogo24_r5rpfu.png',
-    website: 'https://kontour.ch',
-    description: 'UX Design auf höchstem Niveau'
+    id: "kensignton",
+    name: "Kontour Design Studio",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1739987898/klogo24_r5rpfu.png",
+    website: "https://kontour.ch",
+    description: "UX Design auf höchstem Niveau",
   },
   {
-    id: 'swiss-immo',
-    name: 'Headz',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1739987887/WhatsApp_Image_2025-02-19_at_20.12.33_k68bd0.jpg',
-    website: 'https://www.headz-agency.ch',
-    description: 'All-in-One Agentur für Influencer'
+    id: "swiss-immo",
+    name: "Headz",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1739987887/WhatsApp_Image_2025-02-19_at_20.12.33_k68bd0.jpg",
+    website: "https://www.headz-agency.ch",
+    description: "All-in-One Agentur für Influencer",
   },
   {
-    id: 'artemia',
-    name: 'Webpiranha',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1739987876/WhatsApp_Image_2025-02-19_at_20.15.45_kiostz.jpg',
-    website: 'https://www.webpiranha.ch',
-    description: 'Ihr Schweizer Webentwickler'
+    id: "artemia",
+    name: "Webpiranha",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1739987876/WhatsApp_Image_2025-02-19_at_20.15.45_kiostz.jpg",
+    website: "https://www.webpiranha.ch",
+    description: "Ihr Schweizer Webentwickler",
   },
   {
-    id: 'rankist',
-    name: 'Rankist',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1740987107/logo_neu_rankist_phv50a.png',
-    website: 'https://rankist.ch',
-    description: 'SEO und E-Commerce Spezialist für Shopify'
+    id: "rankist",
+    name: "Rankist",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1740987107/logo_neu_rankist_phv50a.png",
+    website: "https://rankist.ch",
+    description: "SEO und E-Commerce Spezialist für Shopify",
   },
   {
-    id: 'it4int',
-    name: 'IT4INT',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1740987065/it4intlogo_ryhzpo.png',
-    website: 'https://it4int.ch',
-    description: 'IT-Sicherheit und Penetrationstests für KMU'
+    id: "it4int",
+    name: "IT4INT",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1740987065/it4intlogo_ryhzpo.png",
+    website: "https://it4int.ch",
+    description: "IT-Sicherheit und Penetrationstests für KMU",
   },
   {
-    id: 'youstream',
-    name: 'YouStream',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1740986963/youstream-logo_drqquj.svg',
-    website: 'https://youstream.ch',
-    description: 'Professionelle Videoproduktion'
-  }
+    id: "youstream",
+    name: "YouStream",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1740986963/youstream-logo_drqquj.svg",
+    website: "https://youstream.ch",
+    description: "Professionelle Videoproduktion",
+  },
 ];
 
 export const regionalPartners: FrontendPartner[] = [
   {
-    id: 'keller-immobilien',
-    name: 'Keller Immobilien & Treuhand',
-    logo: 'https://cdn.prod.website-files.com/620b69625cf399e569d2c9e6/62f26d021df59435d299d830_keller_logo_800.svg',
-    website: 'https://www.kit.ch',
-    description: 'Immobilienverwaltung und -beratung'
+    id: "keller-immobilien",
+    name: "Keller Immobilien & Treuhand",
+    logo: "https://cdn.prod.website-files.com/620b69625cf399e569d2c9e6/62f26d021df59435d299d830_keller_logo_800.svg",
+    website: "https://www.kit.ch",
+    description: "Immobilienverwaltung und -beratung",
   },
   {
-    id: 'hz-architekten',
-    name: 'HZ Architekten',
-    logo: 'https://static.wixstatic.com/media/66aaab_76073ef8029a4a0d8f786228b9fd89b4~mv2.png',
-    website: 'https://www.hz-architekten.com',
-    description: 'Architektur- und Bauplanungslösungen'
+    id: "hz-architekten",
+    name: "HZ Architekten",
+    logo: "https://static.wixstatic.com/media/66aaab_76073ef8029a4a0d8f786228b9fd89b4~mv2.png",
+    website: "https://www.hz-architekten.com",
+    description: "Architektur- und Bauplanungslösungen",
   },
   {
-    id: 'pawex',
-    name: 'Pawex',
-    logo: 'https://pawex.ch/gallery_gen/c7744c50024067bcf363fa9665956ec3_624x266_fit.png',
-    website: 'https://pawex.ch',
-    description: 'Versicherungsberatung'
+    id: "pawex",
+    name: "Pawex",
+    logo: "https://pawex.ch/gallery_gen/c7744c50024067bcf363fa9665956ec3_624x266_fit.png",
+    website: "https://pawex.ch",
+    description: "Versicherungsberatung",
   },
   {
-    id: 'e-beam',
-    name: 'e-beam',
-    logo: 'https://static.wixstatic.com/media/0c82d3_7c2da54581a342689af1c28bc231318f~mv2.jpeg',
-    website: 'https://www.e-beam.ch',
-    description: 'Elektro-Roller und nachhaltige Mobilität'
+    id: "e-beam",
+    name: "e-beam",
+    logo: "https://static.wixstatic.com/media/0c82d3_7c2da54581a342689af1c28bc231318f~mv2.jpeg",
+    website: "https://www.e-beam.ch",
+    description: "Elektro-Roller und nachhaltige Mobilität",
   },
   {
-    id: 'webagentur-forster',
-    name: 'Webagentur Forster',
-    logo: 'https://www.webagentur-forster.ch/wp-content/uploads/2024/08/webagentur-forster-logo-website-transparent.png.webp',
-    website: 'https://www.webagentur-forster.ch',
-    description: 'Full-Service-Digitalagentur für KMU'
+    id: "webagentur-forster",
+    name: "Webagentur Forster",
+    logo: "https://www.webagentur-forster.ch/wp-content/uploads/2024/08/webagentur-forster-logo-website-transparent.png.webp",
+    website: "https://www.webagentur-forster.ch",
+    description: "Full-Service-Digitalagentur für KMU",
   },
   {
-    id: 'gerust-schweiz',
-    name: 'Gerüst Schweiz',
-    logo: 'https://geruestschweiz.ch/wp-content/uploads/2022/05/Logo-Gerust-Schweiz.png',
-    website: 'https://geruestschweiz.ch',
-    description: 'Gerüstlösungen für Bauprojekte'
+    id: "gerust-schweiz",
+    name: "Gerüst Schweiz",
+    logo: "https://geruestschweiz.ch/wp-content/uploads/2022/05/Logo-Gerust-Schweiz.png",
+    website: "https://geruestschweiz.ch",
+    description: "Gerüstlösungen für Bauprojekte",
   },
   {
-    id: 'umzug-schweiz',
-    name: 'Umzug Schweiz 24',
-    logo: 'https://umzugschweiz24.ch/wp/wp-content/uploads/2017/12/logo-1.png',
-    website: 'https://umzugschweiz24.ch',
-    description: 'Professionelle Umzugsdienstleistungen'
+    id: "umzug-schweiz",
+    name: "Umzug Schweiz 24",
+    logo: "https://umzugschweiz24.ch/wp/wp-content/uploads/2017/12/logo-1.png",
+    website: "https://umzugschweiz24.ch",
+    description: "Professionelle Umzugsdienstleistungen",
   },
   {
-    id: 'werbwerk',
-    name: 'Werbwerk',
-    logo: 'https://werbwerk.ch/wp-content/uploads/2018/08/logo-werbwerk_rgb_poster_-2.png',
-    website: 'https://werbwerk.ch',
-    description: 'Kreative Marketing- und Werbelösungen'
+    id: "werbwerk",
+    name: "Werbwerk",
+    logo: "https://werbwerk.ch/wp-content/uploads/2018/08/logo-werbwerk_rgb_poster_-2.png",
+    website: "https://werbwerk.ch",
+    description: "Kreative Marketing- und Werbelösungen",
   },
   {
-    id: 'chaeserstatt',
-    name: 'Chaeserstatt',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1741748980/logo_chaeserstatt_frehxo.svg',
-    website: 'https://www.chaeserstatt.ch/en/425.html',
-    description: 'Das Seminarhotel im Wallis'
+    id: "chaeserstatt",
+    name: "Chaeserstatt",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1741748980/logo_chaeserstatt_frehxo.svg",
+    website: "https://www.chaeserstatt.ch/en/425.html",
+    description: "Das Seminarhotel im Wallis",
   },
   {
-    id: 'wechselstube',
-    name: 'Wechselstube',
-    logo: 'https://kmu-verein.ch/lovable-uploads/partners/wechselstube.jpg',
-    website: 'http://www.wechselstube.ch',
-    description: 'Online-Geldwechsel für KMUs'
+    id: "wechselstube",
+    name: "Wechselstube",
+    logo: "https://kmu-verein.ch/lovable-uploads/partners/wechselstube.jpg",
+    website: "http://www.wechselstube.ch",
+    description: "Online-Geldwechsel für KMUs",
   },
   {
-    id: 'mltechsolutions',
-    name: 'Loosli TechSolutions',
-    logo: 'https://kmu-verein.ch/lovable-uploads/mltechsolutions.ch.jpg',
-    website: 'https://www.mltechsolutions.ch/',
-    description: 'Digitale OT Transformation'
-  }
+    id: "mltechsolutions",
+    name: "Loosli TechSolutions",
+    logo: "https://kmu-verein.ch/lovable-uploads/mltechsolutions.ch.jpg",
+    website: "https://www.mltechsolutions.ch/",
+    description: "Digitale OT Transformation",
+  },
 ];
 
 export const cooperationPartners: FrontendPartner[] = [
   {
-    id: 'raiffeisen',
-    name: 'Raiffeisen',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1740987898/raiffeisen_logo_dm4tjo.png',
-    website: 'https://www.raiffeisen.ch',
-    description: 'Bank- und Finanzdienstleistungen'
+    id: "raiffeisen",
+    name: "Raiffeisen",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1740987898/raiffeisen_logo_dm4tjo.png",
+    website: "https://www.raiffeisen.ch",
+    description: "Bank- und Finanzdienstleistungen",
   },
   {
-    id: 'wir-bank',
-    name: 'WIR Bank',
-    logo: 'https://www.wir.ch/typo3conf/ext/wir/Templates/Wir/Resources/Public/Icons/logo.svg',
-    website: 'https://www.wir.ch',
-    description: 'Innovative Finanzlösungen'
+    id: "wir-bank",
+    name: "WIR Bank",
+    logo: "https://www.wir.ch/typo3conf/ext/wir/Templates/Wir/Resources/Public/Icons/logo.svg",
+    website: "https://www.wir.ch",
+    description: "Innovative Finanzlösungen",
   },
   {
-    id: 'meinjurist',
-    name: 'meinJurist',
-    logo: 'https://meinjurist.ch/wp-content/uploads/2021/03/meinJurist_logo_360p.png',
-    website: 'https://meinjurist.ch',
-    description: 'Juristische Dienstleistungen'
-  }
+    id: "meinjurist",
+    name: "meinJurist",
+    logo: "https://meinjurist.ch/wp-content/uploads/2021/03/meinJurist_logo_360p.png",
+    website: "https://meinjurist.ch",
+    description: "Juristische Dienstleistungen",
+  },
 ];
 
 export const patronagePartners: FrontendPartner[] = [
   {
-    id: 'energie-cluster',
-    name: 'Energie-Cluster',
-    logo: 'https://a.storyblok.com/f/111983/2145x231/62b3dd2c58/energie-cluster-logo.png',
-    website: 'https://energie-cluster.ch',
-    description: 'Netzwerk für Energielösungen',
-    customBackground: 'bg-gray-800'
+    id: "energie-cluster",
+    name: "Energie-Cluster",
+    logo: "https://a.storyblok.com/f/111983/2145x231/62b3dd2c58/energie-cluster-logo.png",
+    website: "https://energie-cluster.ch",
+    description: "Netzwerk für Energielösungen",
+    customBackground: "bg-gray-800",
   },
   {
-    id: 'chdu',
-    name: 'CHDU',
-    logo: 'https://res.cloudinary.com/dphbnwjtx/image/upload/v1741748925/chdu-logo_dyyden.avif',
-    website: 'https://www.chdu.ch/',
-    description: 'Dachverband für Unternehmensnachfolge'
+    id: "chdu",
+    name: "CHDU",
+    logo: "https://res.cloudinary.com/dphbnwjtx/image/upload/v1741748925/chdu-logo_dyyden.avif",
+    website: "https://www.chdu.ch/",
+    description: "Dachverband für Unternehmensnachfolge",
   },
   {
-    id: 'KMUFachforum',
-    name: 'KMU Fachforum 2025',
-    logo: 'https://kmu-fachforum.ch/wp-content/uploads/2024/04/one-fachforum-Konferenz-IT-Business-Software-topsoft-digitalrat.png',
-    website: 'https://kmu-fachforum.ch/',
-    description: 'Das praxisnahe Fachforum'
-  }
+    id: "KMUFachforum",
+    name: "KMU Fachforum 2025",
+    logo: "https://kmu-fachforum.ch/wp-content/uploads/2024/04/one-fachforum-Konferenz-IT-Business-Software-topsoft-digitalrat.png",
+    website: "https://kmu-fachforum.ch/",
+    description: "Das praxisnahe Fachforum",
+  },
+  {
+    id: "schweizerDigitalrat",
+    name: "Schweizer Digitalrat",
+    logo: "https://cdn-djckc.nitrocdn.com/IIYspxNcOvzWRERdnJHFOzfnoHJCTxVq/assets/images/optimized/rev-4d81362/www.digitalrat.ch/wp-content/uploads/2022/11/cropped-Digitalrat-Beirat.png",
+    website: "https://digitalrat.ch",
+    description: "Schweizer Digitalrat",
+  },
 ];
